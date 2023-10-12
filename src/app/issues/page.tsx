9c7fc44d39ebd822f4989d353b320bd7511780c8
Halil Tunc <hitunc@outlook.com>
2023-10-12 12:14:00 +0300

    feat: meta data add

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -1,7 +1,13 @@
+import { Metadata } from "next";
+
 import { getGithubIssues } from "@/services/endpoints";
-import Table from "../components/Table/Table";
 import IssuesTable from "./components/IssuesTable/IssuesTable";
 import Pagination from "../components/Pagination/Pagination";
+
+export const metadata: Metadata = {
+  title: "Issues · facebook/react",
+  description: "...",
+};
 
 const IssuesPage = async () => {
   const res = await getGithubIssues();
