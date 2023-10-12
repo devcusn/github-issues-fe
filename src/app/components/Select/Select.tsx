bacc46f0a8e0136a9b736bfbcacffa24b76fe19d
Halil Tunc <hitunc@outlook.com>
2023-10-12 23:02:58 +0300

    feat: select component popper header add

--- src/app/components/Select/Select.tsx
+++ src/app/components/Select/Select.tsx
@@ -3,6 +3,7 @@ import { SelectProps } from "./types";
 import classes from "./style.module.css";
 import DropDownIcon from "@/assets/icons/DropDownIcon";
 import useToggle from "@/hooks/useToggle";
+import CloseIcon from "@/assets/icons/CloseIcon";
 
 const Select: React.FunctionComponent<SelectProps> = ({
   title,
@@ -26,8 +27,18 @@ const Select: React.FunctionComponent<SelectProps> = ({
             onClick={toggleHandler}
           />
           <div className={classes.select_popper}>
-            <div>{`Filter By ${title}`} close</div>
-            <input className={classes.filter_input} />
+            <div className={classes.popper_header}>
+              <span className={classes.popper_title}>
+                {`Filter By ${title}`}
+              </span>
+              <button onClick={toggleHandler}>
+                <CloseIcon />
+              </button>
+            </div>
+            <div className={classes.popper_input}>
+              <input placeholder="Filter" className={classes.filter_input} />
+            </div>
+
             <div className={classes.select_options}>
               {options?.map((o) => (
                 <div key={o.value}>{o.title}</div>
