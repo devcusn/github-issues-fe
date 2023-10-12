9d35447d2d0fad6bc6bfad632c660a98e1eb277a
Halil Tunc <hitunc@outlook.com>
2023-10-12 17:27:00 +0300

    feat: main page edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,30 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="issues">Go Issues</Link>
+    <main className="flex min-h-screen flex-col items-center gap-20 p-24">
+      <p className="text-4xl">Github Issues And Detail Page Frontend Clone</p>
+      <Link className="underline" href="issues">
+        Go Issues
+      </Link>
+      <div>
+        <ul>
+          <span className="font-semibold"> All Routes</span>
+          <li>/issues</li>
+          <li>/issues/[issue_number]</li>
+        </ul>
+        <ul className="mt-10">
+          <span className="font-semibold"> Public Github APIs</span>
+          <li>
+            https://api.github.com/repos/facebook/react/issues?page=[page]
+          </li>
+          <li>
+            https://api.github.com/repos/facebook/react/issues/[issue_number]
+          </li>
+          <li>
+            https://api.github.com/repos/facebook/react/issues/[issue_number]/comments
+          </li>
+        </ul>
+      </div>
     </main>
   );
 }
