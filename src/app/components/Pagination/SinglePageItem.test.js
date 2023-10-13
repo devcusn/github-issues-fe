5c5c8c5ac24eb36b9f5a77260d3717000d9a1c63
Halil Tunc <hitunc@outlook.com>
2023-10-13 08:38:17 +0300

    feat: update test for single page item

--- src/app/components/Pagination/SinglePageItem.test.js
+++ src/app/components/Pagination/SinglePageItem.test.js
@@ -1,21 +1,48 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SinglePageItem from "./SinglePageItem";
 
-describe("SinglePageItem Component", () => {
-  it("renders with correct props", () => {
-    const { getByText } = render(
-      <SinglePageItem page={1} isSelected={false} />
-    );
+const mockPage = "1";
+const mockSelected = false;
 
-    const singlePageItem = getByText(1);
-    expect(singlePageItem).toBeInTheDocument();
-  });
+test("renders SinglePageItem component", () => {
+  render(<SinglePageItem page={mockPage} isSelected={mockSelected} />);
 
-  it("renders without crashing", () => {
-    const { getByText } = render(<SinglePageItem page={1} isSelected={true} />);
+  const pageElement = screen.getByText(mockPage);
+  expect(pageElement).toBeInTheDocument();
 
-    const singlePageItem = getByText("1");
-    expect(singlePageItem).toBeInTheDocument();
-  });
+  const linkElement = screen.getByRole("link");
+  expect(linkElement).toHaveAttribute("href", "/issues?page=TestPage");
+
+  expect(linkElement).not.toHaveClass("single_page_item__selected");
+});
+
+test("renders SinglePageItem component when selected", () => {
+  render(<SinglePageItem page={mockPage} isSelected={true} />);
+
+  const linkElement = screen.getByRole("link");
+  expect(linkElement).toHaveClass("single_page_item__selected");
+});
+
+test("renders SinglePageItem component with string page", () => {
+  render(<SinglePageItem page="TestPage" isSelected={mockSelected} />);
+
+  const linkElement = screen.getByRole("link");
+  expect(linkElement).toHaveAttribute("href", "");
+});
+
+test("handles click event correctly", () => {
+  const onClick = jest.fn();
+  render(
+    <SinglePageItem
+      page={mockPage}
+      isSelected={mockSelected}
+      onClick={onClick}
+    />
+  );
+
+  userEvent.click(screen.getByRole("link"));
+
+  expect(onClick).toHaveBeenCalledTimes(1);
 });
